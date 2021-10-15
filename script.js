d3c10cd3a993d89f182761f1e4e0f5fa8c5f76d1
Chris Insignares <insignaresc115@gmail.com>
2021-10-15 00:39:24 -0400

    added prompt

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// returns the first "ELEMENT" (generate?) within the document that matches the specified selector
+// line 28 in html
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +9,35 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var generateBtn;
+
+function generatePassword() {
+  var passwordLength = prompt("pick a pw length, 8-128");
+}
+
+console.log(generatePassword);
+// function generatePassword() {
+//   var passwordLength = prompt(
+//     "Please Enter How Long You Want Your Password to Be (Between 8 and 128 characters)"
+//   );
+// }
+
+// variable for all alphabets
+// list and then function to split space
+// function to lower case and upper case
+
+// write out variable with numbers
+// write out special characters
+
+// ask user for apsswrod length
+// prompt to choose alphabets
+// prompt to choose upper case
+// prompt to choose lower case
+// prompt to choose specials
+
+//
